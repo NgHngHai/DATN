2c51f2bfe4014d886cc594cfbc723ffa623aace1
naiveBoy <teinhng@gmail.com>
2025-12-21 18:17:45 +0700

    update KEY_MAPPING

--- Assets/Scripts/UI/ControlsController.cs
+++ Assets/Scripts/UI/ControlsController.cs
@@ -2,18 +2,30 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Utilities;
 
 public class ControlsController : MonoBehaviour
 {
     [Header("Reference")]
-    public Image imgMapping0;
-    public Image imgMapping1, imgMapping2, imgMapping3, imgMapping4, imgMapping5, imgMapping6;
-    public Image imgMapping7, imgMapping8, imgMapping9, imgMapping10, imgMapping11, imgMapping12;
+    public ControlsButton imgMapping0;
+    public ControlsButton imgMapping1, imgMapping2, imgMapping3, imgMapping4, imgMapping5, imgMapping6;
+    public ControlsButton imgMapping7, imgMapping8, imgMapping9, imgMapping10, imgMapping11, imgMapping12;
     [Header("Data")]
     public Sprite shortBtnUnselectedSprite;
     public Sprite shortBtnSelectedSprite, longBtnUnselectedSprite, longBtnSelectedSprite;
+    [Header("input Action")]
+    public InputActionReference moveActionRef;
+    public InputActionReference jumpActionRef, attackActionRef, parryActionRef, dashActionRef, skillActionRef, healActionRef, interactActionRef;
+
     int selectingControlId;
-    List<Image> imgMappings = new();
+    List<ControlsButton> imgMappings = new();
+    List<GameObject> goRebindingBoxes = new();
+    List<TextMeshProUGUI> txtRebindingKeys = new();
+
+    System.IDisposable _buttonListener;
+    int step = 0;
+    string keyname;
 
 
     void Awake()
@@ -31,37 +43,135 @@ public class ControlsController : MonoBehaviour
         imgMappings.Add(imgMapping10);
         imgMappings.Add(imgMapping11);
         imgMappings.Add(imgMapping12);
+        for (int i = 0; i < 13; i++)
+        {
+            goRebindingBoxes.Add(imgMappings[i].transform.GetChild(2).gameObject);
+            txtRebindingKeys.Add(goRebindingBoxes[i].transform.GetChild(1).GetComponent<TextMeshProUGUI>());
+        }
 
         selectingControlId = -1;
+        step = 0;
+        keyname = "";
     }
 
 
-    public void FocusButton(int id, int type)
+    public void FocusButton(int id)
     {
-        if (type == 0)
-        {
-            imgMappings[id].sprite = shortBtnSelectedSprite;
-        }
-        else
-        {
-            imgMappings[id].sprite = longBtnSelectedSprite;
-        }
+        _buttonListener = InputSystem.onAnyButtonPress.Call(OnAnyButtonPress);
+        
+        imgMappings[id].ChangeSprite(shortBtnSelectedSprite, longBtnSelectedSprite);
+        goRebindingBoxes[id].SetActive(true);
+        txtRebindingKeys[id].text = "";
 
         if (selectingControlId != -1)
         {
-            if (type == 0)
-            {
-                imgMappings[selectingControlId].sprite = shortBtnUnselectedSprite;
-            }
-            else
-            {
-                imgMappings[selectingControlId].sprite = longBtnUnselectedSprite;
-            }
-            imgMappings[selectingControlId].transform.GetChild(1).GetComponent<TextMeshProUGUI>().color = new Color32(144, 169, 177, 255);
+            imgMappings[selectingControlId].ChangeSprite(shortBtnUnselectedSprite, longBtnUnselectedSprite);
+            goRebindingBoxes[id].SetActive(false);
         }
         selectingControlId = id;
     }
 
 
     public bool IsSelectingControlId(int id) => selectingControlId == id;
+
+
+    public void OnAnyButtonPress(InputControl ic)
+    {
+        if (step == 1)
+        {
+            if (ic.name == "enter")
+            {
+                step = 0;
+                txtRebindingKeys[selectingControlId].text = "";
+                _buttonListener?.Dispose();
+                _buttonListener = null;
+                RebindingAction();
+                return;
+            }
+            else if (ic.name == "backspace")
+            {
+                step = 0;
+                txtRebindingKeys[selectingControlId].text = "";
+                return;
+            }
+        }
+        step = 1;
+        keyname = ic.name;
+        txtRebindingKeys[selectingControlId].text = keyname;
+    }
+
+
+    public void RebindingAction()
+    {
+        switch (selectingControlId)
+        {
+            case 0:
+                break;
+            case 1:
+                break;
+            case 2:
+                moveActionRef.action.ApplyBindingOverride(2, "<Keyboard>/" + keyname);
+                break;
+            case 3:
+                moveActionRef.action.ApplyBindingOverride(3, "<Keyboard>/" + keyname);
+                break;
+            case 4:
+                moveActionRef.action.ApplyBindingOverride("<Keyboard>/" + keyname);
+                break;
+            case 5:
+                UIManager.Instance.ChangeOpenFunctionActionBinding("<Keyboard>/" + keyname);
+                break;
+            case 6:
+                UIManager.Instance.ChangePauseGameActionBinding("<Keyboard>/" + keyname);
+                break;
+            case 7:
+                attackActionRef.action.ApplyBindingOverride("<Keyboard>/" + keyname);
+                break;
+            case 8:
+                // parryActionRef.action.ApplyBindingOverride("<Keyboard>/" + keyname);
+                break;
+            case 9:
+                dashActionRef.action.ApplyBindingOverride("<Keyboard>/" + keyname);
+                break;
+            case 10:
+                skillActionRef.action.ApplyBindingOverride("<Keyboard>/" + keyname);
+                break;
+            case 11:
+                healActionRef.action.ApplyBindingOverride("<Keyboard>/" + keyname);
+                break;
+            case 12:
+                interactActionRef.action.ApplyBindingOverride("<Keyboard>/" + keyname);
+                break;
+        }
+        
+        imgMappings[selectingControlId].transform.GetChild(1).GetComponent<TextMeshProUGUI>().text = keyname;
+        goRebindingBoxes[selectingControlId].SetActive(false);
+        imgMappings[selectingControlId].ChangeSprite(shortBtnUnselectedSprite, longBtnUnselectedSprite);
+        selectingControlId = -1;
+    }
+    
+
+    void OnDisable()
+    {
+        _buttonListener?.Dispose();
+        _buttonListener = null;
+
+        step = 0;
+        keyname = "";
+    }
 }
+
+// public void SwapJumpKeyToEnter()
+// {
+//     // Giả sử action lại là InputActionReference
+//     InputAction action = jumpActionRef.action;
+
+//     // Cách 1: Ghi đè bằng Path string (Phải đúng cú pháp <Device>/Key)
+//     // "<Keyboard>/enter"
+//     // "<Gamepad>/buttonSouth"
+//     action.ApplyBindingOverride("<Keyboard>/enter");
+
+//     // Cách 2: Ghi đè vào một index cụ thể (nếu Action có nhiều binding như Keyboard + Gamepad)
+//     // Index 0 thường là binding đầu tiên trong list
+//     action.ApplyBindingOverride(0, "<Keyboard>/enter");
+// }
