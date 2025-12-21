4ca5f5a7f224da374832630375b686a1f7e45809
NgHngHai <honghaiae@gmail.com>
2025-12-21 19:11:58 +0700

    Update Shop.cs

--- Assets/Scripts/Environment/Shop.cs
+++ Assets/Scripts/Environment/Shop.cs
@@ -3,32 +3,17 @@ using UnityEngine;
 
 public class Shop : Interactables
 {
-    [Header("Shop Dialog")]
-    [Tooltip("Dialog UI instance on a screen-space canvas.")]
-    [SerializeField] private DialogUI dialogUI;
+    private SFXEmitter sfxEmitter;
 
-    [Tooltip("Optional: The Shop UI root to enable when opening the shop. If null, will try tag 'ShopUI'.")]
-    [SerializeField] private GameObject shopUIRoot;
-
-    [TextArea]
-    [SerializeField] private string dialogMessage = "Welcome! Do you want to open the shop?";
-
-    [SerializeField] private string openButtonText = "Open Shop";
-    [SerializeField] private string quitButtonText = "Quit";
-
-    private bool _dialogOpen;
+    protected override void Awake()
+    {
+        base.Awake();
+        sfxEmitter = GetComponent<SFXEmitter>();
+    }
 
     protected override void OnInteract(GameObject player)
     {
+        sfxEmitter.ChangeAndPlaySFXAtHere(0); // Shop open SFX
         UIManager.Instance.OpenShop();
-    }
-
-    private void OnDisable()
-    {
-        // If we get disabled (leaving scene, etc.), make sure dialog isn't left open
-        if (dialogUI != null && dialogUI.IsOpen)
-            dialogUI.Hide();
-
-        _dialogOpen = false;
     }
 }
