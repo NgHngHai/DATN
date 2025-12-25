a5cc67a03c04a1b73e8bce752511d1bca5d777f3
NgHngHai <honghaiae@gmail.com>
2025-12-25 21:01:12 +0700

    Update Shop.cs

--- Assets/Scripts/Environment/Shop.cs
+++ Assets/Scripts/Environment/Shop.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 
 public class Shop : Interactables
 {
+    [SerializeField] private GameObject tutotialPromptEN;
     private SFXEmitter sfxEmitter;
 
     protected override void Awake()
@@ -15,5 +16,18 @@ public class Shop : Interactables
     {
         sfxEmitter.ChangeAndPlaySFXAtHere(0); // Shop open SFX
         UIManager.Instance.OpenShop();
+    }
+
+    new private void OnTriggerEnter2D(Collider2D collision)
+    {
+        base.OnTriggerEnter2D(collision);
+
+        tutotialPromptEN.SetActive(true);
+    }
+
+    new private void OnTriggerExit2D(Collider2D collision)
+    {
+        base.OnTriggerExit2D(collision);
+        tutotialPromptEN.SetActive(false);
     }
 }
